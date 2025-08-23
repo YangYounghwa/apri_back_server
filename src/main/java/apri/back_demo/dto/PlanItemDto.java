6afad69c7a6f0b6af805e0eb10d1ce7d0968f276
YangYounghwa <51239748+YangYounghwa@users.noreply.github.com>
2025-08-24 00:07:37 +0900

    Error fixed in PlanService. Automatically inserts routes and locations for give plans. Automatically infers routes from segmentsJson.

--- src/main/java/apri/back_demo/dto/PlanItemDto.java
+++ src/main/java/apri/back_demo/dto/PlanItemDto.java
@@ -3,6 +3,8 @@ package apri.back_demo.dto;
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import lombok.Data;
 
@@ -17,5 +19,14 @@ public class PlanItemDto {
     private LocalDateTime endTime;
 
     private LocationDto location;
-    private RouteDto route;
+  // --- ANNOTATIONS ADDED HERE ---
+  // This tells the JSON deserializer to automatically detect the correct subclass
+  // based on the fields present in the JSON data. If a "segments" field exists,
+  // it will correctly create a FullRouteDto object.
+  @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+  @JsonSubTypes({
+    @JsonSubTypes.Type(FullRouteDto.class),
+    @JsonSubTypes.Type(RouteDto.class)
+  })
+  private RouteDto route;
 }
